c6a8bc79f0955436a5906e606bdce63f89d2d4d8
liamattard <liam.dratta@gmail.com>
2021-01-19 23:21:42 +0100

    Added description and values

--- public/scripts/data.js
+++ public/scripts/data.js
@@ -8,17 +8,17 @@ function click(d) {
         var shift_value = body_top.toString();
         document.getElementById("body").style.top = shift_value + "px";
         document.getElementById("body").style.height = shift_value + "px";
-        
+
         var shift_hor = move((d.parent.children).length, (d.parent.children).indexOf(d));
-        console.log("din",Math.abs(shift_hor))
+        console.log("din", Math.abs(shift_hor))
         document.getElementById("body").style.left = shift_hor.toString() + "px";
 
 
         body_width = body_width + 200;
-        document.getElementById("body").style.width = body_width.toString()  + "px";
+        document.getElementById("body").style.width = body_width.toString() + "px";
 
         body_height = body_height + 200;
-        document.getElementById("body").style.height = body_height.toString()  + "px";
+        document.getElementById("body").style.height = body_height.toString() + "px";
 
 
 
@@ -27,21 +27,21 @@ function click(d) {
             currentPlayer.nextday();
         }
 
-        var jobs = ["Nursery Teacher", "Head Waiter" , "Boring Office Job"].indexOf(d.name);
-        var apartment = ["Garage", "Small Apartment" , "Nice Apartment"].indexOf(d.name);
+        var jobs = ["Nursery Teacher", "Head Waiter", "Boring Office Job"].indexOf(d.name);
+        var apartment = ["Garage", "Small Apartment", "Nice Apartment"].indexOf(d.name);
 
         if (jobs != -1) {
 
-            currentPlayer.job = jobs +1;
+            currentPlayer.job = jobs + 1;
 
             generateHouses(d);
 
-        } 
+        }
         else if (apartment != -1) {
 
             d["_children"] = Array();
-            currentPlayer.house = apartment+1;
-            generateSampleBlock(d);
+            currentPlayer.house = apartment + 1;
+            carOrBus(d);
 
         } else {
 
@@ -77,7 +77,7 @@ var i = 0,
     rectH = 25;
 
 
-var tree = d3.layout.tree().nodeSize([190, 70]);
+var tree = d3.layout.tree().nodeSize([190, 10]);
 var diagonal = d3.svg.diagonal()
     .projection(function (d) {
         return [d.x + rectW / 2, d.y + rectH / 2];
@@ -85,7 +85,7 @@ var diagonal = d3.svg.diagonal()
 
 console.log(screen.width, screen.height)
 var svg = d3.select("#body").classed("svg-container", true).append("svg")
-.classed("svg-content-responsive", true).attr("width", 7000).attr("height", 7000)
+    .classed("svg-content-responsive", true).attr("width", 7000).attr("height", 7000)
     .call(zm = d3.behavior.zoom().scaleExtent([1, 3]).on("zoom", null)).append("g")
     .attr("transform", "translate(" + screen.width / 2.5 + "," + screen.height / 7 + ")scale(2.5,2.5)");
 
@@ -106,50 +106,53 @@ function collapse(d) {
 root.children.forEach(collapse);
 update(root);
 
+
+
 d3.select("#body").style("height", screen.height);
 
 
 
 function update(source) {
 
+
     // Compute the new tree layout.
     var nodes = tree.nodes(root).reverse(),
         links = tree.links(nodes);
-  
+
     // Normalize for fixed-depth.
     nodes.forEach(function (d) {
         d.y = d.depth * 180;
     });
-  
+
     // Update the nodes…
     var node = svg.selectAll("g.node")
         .data(nodes, function (d) {
-        return d.id || (d.id = ++i);
-    });
-  
+            return d.id || (d.id = ++i);
+        });
+
     // Enter any new nodes at the parent's previous position.
     var nodeEnter = node.enter().append("g")
         .attr("class", "node")
         .attr("transform", function (d) {
-        return "translate(" + source.x0 + "," + source.y0 + ")";
-    })
+            return "translate(" + source.x0 + "," + source.y0 + ")";
+        })
         .on("click", click);
-  
-        
-  
+
+
+
     nodeEnter.append("rect")
         .attr("class", "title")
-        .attr("width", rectW*1.5)
-      //   .attr("height", "25px")
+        .attr("width", rectW * 1.5)
+        //   .attr("height", "25px")
         .attr("rx", 6)
         .attr("ry", 6)
         .attr("stroke", "gray")
         .attr("stroke-width", 1)
         .style("fill", "#ffff");
-  
-        nodeEnter.append("rect")
-        .attr("width", rectW*2)
-        .attr("height", rectH*2)
+
+    nodeEnter.append("rect")
+        .attr("width", rectW * 2)
+        .attr("height", rectH * 2)
         .attr("rx", 6)
         .attr("ry", 6)
         .attr("x", -50)
@@ -157,182 +160,183 @@ function update(source) {
         .attr("stroke", "gray")
         .attr("stroke-width", 1)
         .style("fill", "#ffff");
-    
-  
-        nodeEnter.append("text")
+
+
+    nodeEnter.append("text")
         .attr("x", rectW / 2)
         .attr("y", rectH / 2)
         .attr("dy", ".35em")
         .attr("text-anchor", "middle")
         .text(function (d) {
-        return d.name;
-    });
-  
+            return d.name;
+        });
+
     nodeEnter.append("text")
         .attr("class", "description")
         .attr("x", rectW / 2)
-        .attr("y", rectH*2)
+        .attr("y", rectH * 2)
         .attr("dy", ".35em")
         .attr("text-anchor", "middle")
         .text(function (d) {
-        return "U aqbad u ixtri din id-dar chicken";
-    }) ;
-  
-  
+            return d.description;
+        });
+
+
     nodeEnter.append("text")
         .attr("class", "attributes")
         .attr("x", -rectW / 3)
-        .attr("y", rectH*3.1)
+        .attr("y", rectH * 3.1)
         .attr("dy", ".35em")
         .attr("text-anchor", "middle")
         .text(function (d) {
-        return "Health";
-    }) ;
+            return "Health";
+        });
     nodeEnter.append("text")
         .attr("class", "attribute_values")
         .attr("x", -rectW / 3)
-        .attr("y", rectH*2.8)
+        .attr("y", rectH * 2.8)
         .attr("dy", ".35em")
         .attr("fill", " rgb(100, 195, 50)")
         .attr("text-anchor", "middle")
         .text(function (d) {
-        return "+10";
-    }) ;
-  
-  
-  
-  
+            return d.Health;
+        });
+
+    var x = true;
+
     nodeEnter.append("text")
         .attr("class", "attributes")
-        .attr("x", "105px" )
-        .attr("y", rectH*3.1)
+        .attr("x", "105px")
+        .attr("y", rectH * 3.1)
         .attr("dy", ".35em")
         .attr("text-anchor", "middle")
         .text(function (d) {
-        return "Happiness";
-    }) ;
+            if (d.Happiness== null){
+                x=false;
+            }
+            return "Happiness";
+        });
     nodeEnter.append("text")
         .attr("class", "attribute_values")
-        .attr("x", "105px" )
-        .attr("y", rectH*2.8)
+        .attr("x", "105px")
+        .attr("y", rectH * 2.8)
         .attr("dy", ".35em")
         .attr("fill", " rgb(131, 25, 25)")
         .attr("text-anchor", "middle")
         .text(function (d) {
-        return "-10";
-    }) ;
-  
-  
-   
-  
+            return  d.Happiness;
+        });
+
     nodeEnter.append("text")
-    .attr("class", "attributes")
-    .attr("x", rectW / 2.3)
-    .attr("y", rectH*3.1)
-    .attr("dy", ".35em")
-    .attr("text-anchor", "middle")
-    .text(function (d) {
-    return "Money";
-   }) ;
-   nodeEnter.append("text")
-   .attr("class", "attribute_values")
-   .attr("x", rectW / 2.3)
-   .attr("y", rectH*2.8)
-   .attr("dy", ".35em")
-   .attr("text-anchor", "middle")
-   .attr("fill", "rgb(100, 195, 50)")
-   .text(function (d) {
-   return "+10";
-  }) ;
-  
+        .attr("class", "attributes")
+        .attr("x", rectW / 2.3)
+        .attr("y", rectH * 3.1)
+        .attr("dy", ".35em")
+        .attr("text-anchor", "middle")
+        .text(function (d) {
+            return "Money";
+        });
+
+    nodeEnter.append("text")
+        .attr("class", "attribute_values")
+        .attr("x", rectW / 2.3)
+        .attr("y", rectH * 2.8)
+        .attr("dy", ".35em")
+        .attr("text-anchor", "middle")
+        .attr("fill", "rgb(100, 195, 50)")
+        .text(function (d) {
+            return d.Money;
+        });
+
+
     // Transition nodes to their new position.
     var nodeUpdate = node.transition()
         .duration(duration)
         .attr("transform", function (d) {
-        return "translate(" + d.x + "," + d.y + ")";
-    });
-  
+            return "translate(" + d.x + "," + d.y + ")";
+        });
+
     nodeUpdate.select("rect")
         .attr("width", rectW)
         .attr("height", rectH)
-       
+
         .attr("rx", 6)
         .attr("ry", 6)
         .attr("stroke", "gray")
         .attr("stroke-width", 1)
-        .style("fill",  "#fff");
-  
+        .style("fill", "#fff");
+
     nodeUpdate.select("text")
         .style("fill-opacity", 1);
-  
+
     // Transition exiting nodes to the parent's new position.
     var nodeExit = node.exit().transition()
         .duration(duration)
         .attr("transform", function (d) {
-        return "translate(" + source.x + "," + source.y + ")";
-    })
+            return "translate(" + source.x + "," + source.y + ")";
+        })
         .remove();
-  
+
     nodeExit.select("rect")
         .attr("width", rectW)
         .attr("height", rectH)
-      //   .attr("width", bbox.getBBox().width)
-      //   .attr("height", bbox.getBBox().height)
+        //   .attr("width", bbox.getBBox().width)
+        //   .attr("height", bbox.getBBox().height)
         .attr("rx", 6)
         .attr("ry", 6)
         .attr("stroke", "gray")
         .attr("stroke-width", 1);
-  
+
     nodeExit.select("text");
-  
+
     // Update the links…
     var link = svg.selectAll("path.link")
         .data(links, function (d) {
-        return d.target.id;
-    });
-  
+            return d.target.id;
+        });
+
     // Enter any new links at the parent's previous position.
     link.enter().insert("path", "g")
         .attr("class", "link")
         .attr("x", rectW / 2)
         .attr("y", rectH / 2)
         .attr("d", function (d) {
-        var o = {
-            x: source.x0,
-            y: source.y0
-        };
-        return diagonal({
-            source: o,
-            target: o
+            var o = {
+                x: source.x0,
+                y: source.y0
+            };
+            return diagonal({
+                source: o,
+                target: o
+            });
         });
-    });
-  
+
     // Transition links to their new position.
     link.transition()
         .duration(duration)
         .attr("d", diagonal);
-  
-  //   Transition exiting nodes to the parent's new position.
+
+    //   Transition exiting nodes to the parent's new position.
     link.exit().transition()
         .duration(duration)
         .attr("d", function (d) {
-        var o = {
-            x: source.x,
-            y: source.y
-        };
-        return diagonal({
-            source: o,
-            target: o
-        });
-    })
+            var o = {
+                x: source.x,
+                y: source.y
+            };
+            return diagonal({
+                source: o,
+                target: o
+            });
+        })
         .remove();
-  
+
     // Stash the old positions for transition.
     nodes.forEach(function (d) {
         d.x0 = d.x;
         d.y0 = d.y;
     });
-  }
+}
 
 var audio_btn = new Audio('button.mp3');
 var audio_move = new Audio('move.mp3');
@@ -342,23 +346,23 @@ function redraw() {
     //console.log("here", d3.event.translate, d3.event.scale);
     svg.attr("transform",
         "translate(" + d3.event.translate + ")"
-        + " scale(" + d3.event.scale + ")").attr("width",100+"%");
+        + " scale(" + d3.event.scale + ")").attr("width", 100 + "%");
 }
 
 
-function move(length, index){
-    if(length == 2){
-        if(index == 0){
+function move(length, index) {
+    if (length == 2) {
+        if (index == 0) {
             return 220
-        }else{
+        } else {
             return -200
         }
-    }else if(length == 3){
-        if(index == 0){
+    } else if (length == 3) {
+        if (index == 0) {
             return 500
-        }else if(index == 2){
+        } else if (index == 2) {
             return -500
-        }else{
+        } else {
             return 0
         }
     }
--- public/scripts/generateStory.js
+++ public/scripts/generateStory.js
@@ -4,22 +4,39 @@ function generateJobs(){
     var root = new Object();
 
     root["name"] = "Choose Your Job";
+    root["description"] = "Choose Carefully!";
     root["children"] = [];
+    root["noValues"] = true;
 
     var Nursery = new Object();
     Nursery["name"] = "Nursery Teacher";
     Nursery["children"] = [];
+    Nursery["description"] = "Take care of toddlers."
+    Nursery["Happiness"] = "+10";
+    Nursery["Money"] = "+70/ 7 days";
+    Nursery["Health"] = "+0";
     Nursery.changeDay = false;
+    Nursery["noValues"] = false;
      
     var HeadWaiter = new Object();
     HeadWaiter["name"] = "Head Waiter";
     HeadWaiter["children"] = [];
+    HeadWaiter["description"] = "Managing the whole restaurant."
+    HeadWaiter["Happiness"] = "+0";
+    HeadWaiter["Money"] = "+140/ 7 days";
+    HeadWaiter["Health"] = "+0";
+    HeadWaiter["noValues"] = true;
     HeadWaiter.changeDay = false;
 
     var OfficeJob = new Object();
     OfficeJob["name"] = "Boring Office Job";
     OfficeJob["children"] = [];
+    OfficeJob["description"] = "Drinking coffee and counting money.";
+    OfficeJob["Happiness"] = "-10";
+    OfficeJob["Money"] = "+200/ 7 days";
+    OfficeJob["Health"] = "+0";
     OfficeJob.changeDay = false;
+    OfficeJob["noValues"] = false;
 
     root.children.push(Nursery);
     root.children.push(HeadWaiter);
@@ -42,8 +59,9 @@ function generateSampleBlock(root){
 function generateHouses(root){
 
     var option = new Object();
-    option["name"] = "Choose Your Accomodation";
+    option["name"] = "Accomodation";
     option["children"] =  [];
+    option["description"] = "Choose where you will call home."
     option.changeDay = false;
 
 
@@ -51,21 +69,74 @@ function generateHouses(root){
     Garage["name"] = "Garage";
     Garage["children"] = Array();
     Garage.changeDay = false;
+    Garage["children"] =  [];
+    Garage["description"] = ""
+    Garage["Happiness"] = "-5";
+    Garage["Money"] = "-35/ 7 days";
+    Garage["Health"] = "+0";
      
     var Small = new Object();
     Small["name"] = "Small Apartment";
     Small["children"] = [];
+    Small["description"] = ""
+    Small["Happiness"] = "+0";
+    Small["Money"] = "-60/ 7 days";
+    Small["Health"] = "+0";
     Small.changeDay = false;
 
     var Nice = new Object();
     Nice["name"] = "Nice Apartment";
     Nice["children"] = [];
+    Nice["description"] = ""
+    Nice["Happiness"] = "+10";
+    Nice["Money"] = "-100/ 7 days";
+    Nice["Health"] = "+0";
     Nice.changeDay = false;
 
     root._children.push(option);
     option.children.push(Garage);
     option.children.push(Small);
     option.children.push(Nice);
+
+    body_top = body_top - screen.height / 2.2;
+    var shift_value = body_top.toString();
+    document.getElementById("body").style.top = shift_value + "px";
+    document.getElementById("body").style.height = shift_value + "px";
+    return root;
+}
+
+
+function carOrBus(root){
+
+    var option = new Object();
+    option["name"] = "Transportation";
+    option["children"] =  [];
+    option["description"] = "Choose your transportation method."
+    option.changeDay = false;
+
+
+    var Bus= new Object();
+    Bus["name"] = "By Bus";
+    Bus["children"] = Array();
+    Bus.changeDay = false;
+    Bus["children"] =  [];
+    Bus["description"] = "Travel to work by bus everyday."
+    Bus["Happiness"] = "-10";
+    Bus["Money"] = "-35/ 7 days";
+    Bus["Health"] = "+0";
+
+    var Car= new Object();
+    Car["name"] = "By Car";
+    Car["children"] = [];
+    Car["description"] = "Travel to work by car everyday."
+    Car["Happiness"] = "+20";
+    Car["Money"] = "-100/ 7 days";
+    Car["Health"] = "+0";
+    Car.changeDay = false;
+
+    root._children.push(option);
+    option.children.push(Car);
+    option.children.push(Bus);
 
     body_top = body_top - screen.height / 2.2;
     var shift_value = body_top.toString();
